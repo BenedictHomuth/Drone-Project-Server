8c8fd206a456b258eed74207a6fd4a8440322cd2
BenedictHomuth <benedict.homuth@gmail.com>
2019-10-12 22:57:15 +0200

    Reworked! User receives table from DB send data.

--- public/js/getData.js
+++ public/js/getData.js
@@ -3,20 +3,57 @@ getData();
             const response = await fetch('/api');
             const data = await response.json();
 
-            //Keyword: DOM-Element
-            //Here we make the send data visible to the user on the webpage
-            for (item of data){
-                const root = document.createElement('div');
-                const geo = document.createElement('div');
-                const date = document.createElement('div');
-                root.append(geo,date);
-                document
+            //Create a table from item
+              function createTable() {
+                var myTableDiv = document.getElementById("myTable");
+              
+                var table = document.createElement('table');
+                table.border = '1';
 
-                geo.textContent = `lat: ${item.lat}°, lon: ${item.lon}°\n`;
-                const dateString = new Date(item.timestamp).toLocaleString();
-                date.textContent = `Date: ${dateString}`;
+                //Start th
+                var tableRow = document.createElement('tr');
 
-                root.append(geo,date);
-                document.body.append(root);
+                var tableHeadOne = document.createElement('th');
+                var tableHeadTwo = document.createElement('th');
+                var tableHeadThree = document.createElement('th');
+
+                tableHeadOne.appendChild(document.createTextNode("Latitude"));
+                tableHeadTwo.appendChild(document.createTextNode("Longitude"));
+                tableHeadThree.appendChild(document.createTextNode("Time"));
+
+                tableRow.appendChild(tableHeadOne);
+                tableRow.appendChild(tableHeadTwo);
+                tableRow.appendChild(tableHeadThree);
+                
+                table.appendChild(tableRow);           
+                //End th
+
+                var tableBody = document.createElement('tbody');
+                table.appendChild(tableBody);
+
+                for(item of data){
+                    var tr = document.createElement('tr');
+                    tableBody.appendChild(tr);
+
+                    var tdOne = document.createElement("td");
+                    var tdTwo = document.createElement("td");
+                    var tdThree = document.createElement("td");
+
+                    tdOne.width = '220';
+                    tdTwo.width = '220';
+                    tdThree.width = '220';
+
+                    tdOne.appendChild(document.createTextNode(`${item.lat}`));
+                    tdTwo.appendChild(document.createTextNode(` ${item.lon}`));
+
+                    const dateString = new Date(item.timestamp).toLocaleString();
+                    tdThree.appendChild(document.createTextNode(dateString));
+
+                    tr.appendChild(tdOne);
+                    tr.appendChild(tdTwo);
+                    tr.appendChild(tdThree);
+                }
+                myTableDiv.appendChild(table);               
             }
+            createTable();            
         }
