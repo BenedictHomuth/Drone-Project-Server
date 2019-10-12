94217dff382605ffc8de120477a6b68440713593
BenedictHomuth <benedict.homuth@gmail.com>
2019-10-12 23:17:32 +0200

    added more css, restructured js

--- public/js/getData.js
+++ public/js/getData.js
@@ -1,59 +1,60 @@
-getData();
-        async function getData() {
-            const response = await fetch('/api');
-            const data = await response.json();
 
-            //Create a table from item
-              function createTable() {
-                var myTableDiv = document.getElementById("myTable");
-              
-                var table = document.createElement('table');
-                table.border = '1';
+async function getData() {
+    const response = await fetch('/api');
+    const data = await response.json();
 
-                //Start th
-                var tableRow = document.createElement('tr');
+    //Create a table from item
+    function createTable() {
+        var myTableDiv = document.getElementById("myTable");
 
-                var tableHeadOne = document.createElement('th');
-                var tableHeadTwo = document.createElement('th');
-                var tableHeadThree = document.createElement('th');
+        var table = document.createElement('table');
+        table.border = '1';
 
-                tableHeadOne.appendChild(document.createTextNode("Latitude"));
-                tableHeadTwo.appendChild(document.createTextNode("Longitude"));
-                tableHeadThree.appendChild(document.createTextNode("Time"));
+        //Start th
+        var tableRow = document.createElement('tr');
 
-                tableRow.appendChild(tableHeadOne);
-                tableRow.appendChild(tableHeadTwo);
-                tableRow.appendChild(tableHeadThree);
-                
-                table.appendChild(tableRow);           
-                //End th
+        var tableHeadOne = document.createElement('th');
+        var tableHeadTwo = document.createElement('th');
+        var tableHeadThree = document.createElement('th');
 
-                var tableBody = document.createElement('tbody');
-                table.appendChild(tableBody);
+        tableHeadOne.appendChild(document.createTextNode("Latitude"));
+        tableHeadTwo.appendChild(document.createTextNode("Longitude"));
+        tableHeadThree.appendChild(document.createTextNode("Time"));
 
-                for(item of data){
-                    var tr = document.createElement('tr');
-                    tableBody.appendChild(tr);
+        tableRow.appendChild(tableHeadOne);
+        tableRow.appendChild(tableHeadTwo);
+        tableRow.appendChild(tableHeadThree);
 
-                    var tdOne = document.createElement("td");
-                    var tdTwo = document.createElement("td");
-                    var tdThree = document.createElement("td");
+        table.appendChild(tableRow);
+        //End th
 
-                    tdOne.width = '220';
-                    tdTwo.width = '220';
-                    tdThree.width = '220';
+        var tableBody = document.createElement('tbody');
+        table.appendChild(tableBody);
 
-                    tdOne.appendChild(document.createTextNode(`${item.lat}`));
-                    tdTwo.appendChild(document.createTextNode(` ${item.lon}`));
+        for (item of data) {
+            var tr = document.createElement('tr');
+            tableBody.appendChild(tr);
 
-                    const dateString = new Date(item.timestamp).toLocaleString();
-                    tdThree.appendChild(document.createTextNode(dateString));
+            var tdOne = document.createElement("td");
+            var tdTwo = document.createElement("td");
+            var tdThree = document.createElement("td");
 
-                    tr.appendChild(tdOne);
-                    tr.appendChild(tdTwo);
-                    tr.appendChild(tdThree);
-                }
-                myTableDiv.appendChild(table);               
-            }
-            createTable();            
+            tdOne.width = '220';
+            tdTwo.width = '220';
+            tdThree.width = '220';
+
+            tdOne.appendChild(document.createTextNode(`${item.lat}`));
+            tdTwo.appendChild(document.createTextNode(` ${item.lon}`));
+
+            const dateString = new Date(item.timestamp).toLocaleString();
+            tdThree.appendChild(document.createTextNode(dateString));
+
+            tr.appendChild(tdOne);
+            tr.appendChild(tdTwo);
+            tr.appendChild(tdThree);
         }
+        myTableDiv.appendChild(table);
+    }
+    createTable();
+}
+getData();
